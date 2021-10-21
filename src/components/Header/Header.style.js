c18cc4785ac5e1b92a0bafebc79fd9db8b85c939
Luizfer-Col <luizfercolorado@gmail.com>
2021-10-21 09:39:57 -0500

    logo color correction

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -1,43 +1,39 @@
-import { StyleSheet } from "react-native";
-import  s from '../../Style'
+import {StyleSheet} from 'react-native';
+import s from '../../Style';
 
 export const styles = StyleSheet.create({
-    container:{
-        alignItems:'center',
-        paddingHorizontal:25,
-        marginTop:'18%',
-    },
-    labelTitle:{
-        display:'flex',
-        flexDirection:'row',
-    },
-    labelLaika:{
-        color:s.color.white,
-        fontSize:s.fs(42),
-        fontFamily: s.ff.ExtraBold,
-        marginBottom:9
-    },
-    labelMembers:{
-        color:s.color.white,
-        fontSize:s.fs(42),
-        fontFamily: s.ff.ExtraBold,
-        marginLeft:-5,
-        marginBottom:9
-
-
-    },
-    textTitle:{
-        fontSize:s.fs(17),
-        color:s.color.white,
-        textAlign:'center',
-        fontFamily:s.ff.Regular,
-        lineHeight: 20
-
-    },
-    card:{
-        width: '88%',
-        marginTop: -30,
-        marginHorizontal:'6%',
-    }
-    
-})
+  container: {
+    alignItems: 'center',
+    paddingHorizontal: 25,
+    marginTop: '18%',
+  },
+  labelTitle: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  labelLaika: {
+    color: s.color.white,
+    fontSize: s.fs(42),
+    fontFamily: s.ff.ExtraBold,
+    marginBottom: 9,
+  },
+  labelMembers: {
+    color: s.color.gold,
+    fontSize: s.fs(42),
+    fontFamily: s.ff.ExtraBold,
+    marginLeft: -5,
+    marginBottom: 9,
+  },
+  textTitle: {
+    fontSize: s.fs(17),
+    color: s.color.white,
+    textAlign: 'center',
+    fontFamily: s.ff.Regular,
+    lineHeight: 20,
+  },
+  card: {
+    width: '88%',
+    marginTop: -40,
+    marginHorizontal: '6%',
+  },
+});
